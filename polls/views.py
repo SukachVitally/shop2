8b1a79b73e902292565aef398ec020ada2732626
Sukach Vitaly <sukach.v.v@gmail.com>
2015-01-27 21:01:15 +0200

    add template

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,19 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
+from polls.models import Poll
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at polls %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of polls %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on polls %s." % poll_id)
--- shop2/urls.py
+++ shop2/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+print(include('polls.urls'))
+
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
