47f505c01077222362561858e9352882a3bed9c0
Sukach Vitaly <sukach.v.v@gmail.com>
2015-01-28 21:20:17 +0200

    add 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Poll
 
@@ -9,11 +9,12 @@ def index(request):
     context = {'latest_poll_list': latest_poll_list}
     return render(request, 'polls/index.html', context)
 
-def detail(request, poll_id):
-    return HttpResponse("You're looking at polls %s." % poll_id)
-
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of polls %s." % poll_id)
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on polls %s." % poll_id)
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
--- shop2/urls.py
+++ shop2/urls.py
@@ -3,9 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-print(include('polls.urls'))
-
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
